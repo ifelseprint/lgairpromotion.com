a55c289009a63206906fab62edaac9b1a5a6dbe3
K <titirat.pa@allianz.com>
2020-12-23 13:08:44 +0700

    Fix : font size font family and resize box list homepage

--- frontend/views/home/_list.php
+++ frontend/views/home/_list.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
 			<div class="homepage-title">
@@ -7,30 +7,37 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-4">
-			<div class="homepage-list homepage-list-img">
-				<img width="100%" src="<?php echo Yii::$app->request->baseUrl; ?>/img/homepage_1.jpg">
-			</div>
-			<div class="homepage-list homepage-list-header">Clean & Cool DualCool DualCare</div>
-			<div class="homepage-list homepage-list-detail">เพียงซื้อเครื่องปรับอากาศ PuriCool Series ทุก BTU 
-			ฟรีบริการล้างเครื่องปรับอากาศพร้อมพ่นน้ำยาฆ่าเชื้อ Covid-19 และแผ่นฟิลเตอร์กรองฝุ่น PM1.0</div>
-			<div class="homepage-list homepage-list-date">ตั้งแต่วันที่ 5 ธันวาคม 2563 – 20 กุมภาพันธ์ 2564</div>
-			<div class="homepage-list homepage-list-button">
-				<a href="<?php echo Yii::$app->request->baseUrl; ?>/cleanandcoolpromotion">ดูเพิ่มเติม</a>
-			</div>
-		</div>
-		<div class="col-md-4" >
-			<div class="homepage-list homepage-list-img">
-				<img width="100%" src="<?php echo Yii::$app->request->baseUrl; ?>/img/homepage_2.jpg" >
-			</div>
-			<div class="homepage-list homepage-list-header">Tiew Tua Thai with LG PuriCare Mini</div>
-			<div class="homepage-list homepage-list-detail">เมื่อซื้อเครื่องฟอกอากาศรุ่น PuriCare Mini รับฟรีกระเป๋าเดินทาง Mini Luggage จำนวน 1 ใบ ฟรี</div>
-			<div class="homepage-list homepage-list-date">ตั้งแต่วันที่ 21 ธันวาคม 2563 – 28 กุมภาพันธ์ 2564</div>
-			<div class="homepage-list homepage-list-button">
-				<a href="<?php echo Yii::$app->request->baseUrl; ?>/tiewtuathaipuricaremini">ดูเพิ่มเติม</a>
+		<div class="col-md-3">
+			<div class="col-md-12">
+				<div class="homepage-list homepage-list-img">
+					<img width="100%" src="<?php echo Yii::$app->request->baseUrl; ?>/img/homepage_1.jpg">
+				</div>
+				<div class="homepage-list homepage-list-header">Clean & Cool DualCool DualCare</div>
+				<div class="homepage-list homepage-list-detail">
+					<div>เพียงซื้อเครื่องปรับอากาศ PuriCool Series ทุก BTU ฟรีบริการล้างเครื่องปรับอากาศพร้อมพ่นน้ำยาฆ่าเชื้อ Covid-19 และแผ่นฟิลเตอร์กรองฝุ่น PM1.0</div>
+					<div class="homepage-list homepage-list-date">ตั้งแต่วันที่ 5 ธันวาคม 2563 – 20 กุมภาพันธ์ 2564</div>
+				</div>
+				<div class="homepage-list homepage-list-button">
+					<a href="<?php echo Yii::$app->request->baseUrl; ?>/cleanandcoolpromotion">ดูเพิ่มเติม</a>
+				</div>
 			</div>
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-3">
+			<div class="col-md-12">
+				<div class="homepage-list homepage-list-img">
+					<img width="100%" src="<?php echo Yii::$app->request->baseUrl; ?>/img/homepage_2.jpg" >
+				</div>
+				<div class="homepage-list homepage-list-header">Tiew Tua Thai with LG PuriCare Mini</div>
+				<div class="homepage-list homepage-list-detail">
+					<div>เมื่อซื้อเครื่องฟอกอากาศรุ่น PuriCare Mini รับฟรีกระเป๋าเดินทาง Mini Luggage จำนวน 1 ใบ ฟรี</div>
+					<div class="homepage-list homepage-list-date">ตั้งแต่วันที่ 21 ธันวาคม 2563 – 28 กุมภาพันธ์ 2564</div>
+				</div>
+				<div class="homepage-list homepage-list-button">
+					<a href="<?php echo Yii::$app->request->baseUrl; ?>/tiewtuathaipuricaremini">ดูเพิ่มเติม</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
 			<!-- <div class="hompage-list hompage-list-img"></div>
 			<div class="hompage-list hompage-list-header"></div>
 			<div class="hompage-list hompage-list-detail"></div>
@@ -39,5 +46,6 @@
 				<a href="">ดูเพิ่มเติม</a>
 			</div> -->
 		</div>
+		<div class="col-md-3"></div>
 	</div>
 </div>
