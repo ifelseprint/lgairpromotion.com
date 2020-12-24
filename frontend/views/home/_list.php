842b891ba68c0f90b87c1ccdf925ab874becb121
K <titirat.pa@allianz.com>
2020-12-24 10:12:51 +0700

    Fix : homepage and fix banner tiew tua thai campaign

--- frontend/views/home/_list.php
+++ frontend/views/home/_list.php
@@ -6,9 +6,9 @@
 			</div>
 		</div>
 	</div>
-	<div class="row">
+	<div class="row d-flex justify-content-center">
 		<div class="col-md-3">
-			<div class="col-md-12">
+			<div class="col-md-12" style="padding: 10px 25px;">
 				<div class="homepage-list homepage-list-img">
 					<img width="100%" src="<?php echo Yii::$app->request->baseUrl; ?>/img/homepage_1.jpg">
 				</div>
@@ -23,7 +23,7 @@
 			</div>
 		</div>
 		<div class="col-md-3">
-			<div class="col-md-12">
+			<div class="col-md-12" style="padding: 10px 25px;">
 				<div class="homepage-list homepage-list-img">
 					<img width="100%" src="<?php echo Yii::$app->request->baseUrl; ?>/img/homepage_2.jpg" >
 				</div>
@@ -37,15 +37,35 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-md-3">
-			<!-- <div class="hompage-list hompage-list-img"></div>
-			<div class="hompage-list hompage-list-header"></div>
-			<div class="hompage-list hompage-list-detail"></div>
-			<div class="hompage-list hompage-list-date"></div>
-			<div class="hompage-list hompage-list-button">
-				<a href="">ดูเพิ่มเติม</a>
-			</div> -->
+		<!-- <div class="col-md-3">
+			<div class="col-md-12" style="padding: 10px 25px;">
+				<div class="homepage-list homepage-list-img">
+					<img width="100%" src="<?php echo Yii::$app->request->baseUrl; ?>/img/homepage_1.jpg">
+				</div>
+				<div class="homepage-list homepage-list-header">Clean & Cool DualCool DualCare</div>
+				<div class="homepage-list homepage-list-detail">
+					<div>เพียงซื้อเครื่องปรับอากาศ PuriCool Series ทุก BTU ฟรีบริการล้างเครื่องปรับอากาศพร้อมพ่นน้ำยาฆ่าเชื้อ Covid-19 และแผ่นฟิลเตอร์กรองฝุ่น PM1.0</div>
+					<div class="homepage-list homepage-list-date">ตั้งแต่วันที่ 5 ธันวาคม 2563 – 20 กุมภาพันธ์ 2564</div>
+				</div>
+				<div class="homepage-list homepage-list-button">
+					<a href="<?php echo Yii::$app->request->baseUrl; ?>/cleanandcoolpromotion">ดูเพิ่มเติม</a>
+				</div>
+			</div>
 		</div>
-		<div class="col-md-3"></div>
+		<div class="col-md-3">
+			<div class="col-md-12" style="padding: 10px 25px;">
+				<div class="homepage-list homepage-list-img">
+					<img width="100%" src="<?php echo Yii::$app->request->baseUrl; ?>/img/homepage_2.jpg" >
+				</div>
+				<div class="homepage-list homepage-list-header">Tiew Tua Thai with LG PuriCare Mini</div>
+				<div class="homepage-list homepage-list-detail">
+					<div>เมื่อซื้อเครื่องฟอกอากาศรุ่น PuriCare Mini รับฟรีกระเป๋าเดินทาง Mini Luggage จำนวน 1 ใบ ฟรี</div>
+					<div class="homepage-list homepage-list-date">ตั้งแต่วันที่ 1 มกราคม 2564 – 28 กุมภาพันธ์ 2564</div>
+				</div>
+				<div class="homepage-list homepage-list-button">
+					<a href="<?php echo Yii::$app->request->baseUrl; ?>/tiewtuathaipuricaremini">ดูเพิ่มเติม</a>
+				</div>
+			</div>
+		</div> -->
 	</div>
 </div>
--- frontend/views/home/index.php
+++ frontend/views/home/index.php
@@ -7,10 +7,12 @@ use frontend\assets\AppAsset;
 use frontend\assets\HomeAsset;
 AppAsset::register($this);
 ?>
-<div class='banner' style="cursor: pointer;">
+<div class='banner desktop' style="cursor: pointer;">
   <img src="<?php echo Yii::$app->request->baseUrl; ?>/img/homepage_banner.jpg" width="100%">
 </div>
-  
+<div class='banner mobile' style="cursor: pointer;">
+  <img src="<?php echo Yii::$app->request->baseUrl; ?>/img/homepage_banner_mobile.jpg" width="100%">
+</div>
 <?php echo $this->render('_list'); ?>
 
 <?php
